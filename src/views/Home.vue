9cc61fc062ec0c5186ced79ce65fc06c21c75fae
Nikitahh <nikitahsieh03@gmail.com>
2025-04-18 14:50:20 +0800

    empty state & subpage content w/ style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -27,13 +27,18 @@
 			</div>
 		</div>
 
-		<!-- Imvoices -->
-		<div>
+		<!-- Invoices -->
+		<div v-if="invoiceData.length > 0">
 			<Invoice
 				v-for="(invoice, index) in invoiceData"
 				:invoice="invoice"
 				:key="index"
 			/>
+		</div>
+		<div v-else class="empty flex flex-column">
+			<img src="../assets/illustration-empty.svg" alt="" />
+			<h3>There is nothing here</h3>
+			<p>Create your new invoice</p>
 		</div>
 	</div>
 </template>
@@ -142,6 +147,27 @@ export default {
 					}
 				}
 			}
+		}
+	}
+
+	.empty {
+		margin-top: 160px;
+		align-items: center;
+		img {
+			width: 214px;
+			height: 200px;
+		}
+		h3 {
+			font-size: 20px;
+			margin-top: 40px;
+		}
+
+		p {
+			text-align: center;
+			max-width: 224px;
+			font-size: 12px;
+			font-weight: 300;
+			margin-top: 16px;
 		}
 	}
 }
